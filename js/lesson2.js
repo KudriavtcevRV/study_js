e38e47bd55301af314f8b5619a241b0e6b00a236
KudriavtcevRV <ruslan_kudriavtcev@mail.ru>
2019-10-19 16:53:12 +0300

    Исправил синтаксис

--- js/lesson2.js
+++ js/lesson2.js
@@ -3,7 +3,7 @@ let num = 266219;
 
 console.log(num + '\n' + '\n' + num.toString().split('').join(' * ') + ' = ' + 
 (eval(num.toString().split('').join(' * '))) + '\n' + '\n' +
-(eval(num.toString().split('').join(' * ')) + '^2 ' + ' = ' + 
-(eval(num.toString().split('').join(' * ')) ** 2)) + '\n' + '\n' +
+(eval(num.toString().split('').join(' * '))) + '^2 ' + ' = ' + 
+((eval(num.toString().split('').join(' * '))) ** 2) + '\n' + '\n' +
 'Выводим первые два числа от результата: ' +
 (eval(num.toString().split('').join(' * ')) ** 2).toString().substring(0, 2)  ); 
