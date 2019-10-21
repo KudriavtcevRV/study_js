4dff480971896ce4efdbb4d3bb5f7c87022448a6
KudriavtcevRV <ruslan_kudriavtcev@mail.ru>
2019-10-22 00:24:45 +0300

    Сделал первое задание третьего урока

--- js/main.js
+++ js/main.js
@@ -1,12 +1,47 @@
-let money = 100;
-let income = 10000;
-let addExpenses = 2000;
-let deposit = 'Gorlovka';
-let mission = 'JS';
-let period = 'Период 16,10,19-24,11,19';
+'use stricts';
 
+let money = prompt('Ваш месячный доход?');
+console.log(money);
 
-alert('Меня зовут Руслан, мне 26 лет, я обучаюсь на курсе JavaScript');
-console.log('Меня зовут Руслан, мне 26 лет, я обучаюсь на курсе JavaScript');
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+console.log(addExpenses.split());
 
+let deposit = Boolean(prompt('Есть ли у вас депозит в банке?'));
+console.log(deposit);
+
+console.log(typeof money);
+console.log(typeof addExpenses);
+console.log(typeof money); 
+
+let question = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+console.log('Какие обязательные ежемесячные расходы у вас есть?: ', question);
+
+let question2 = prompt('Во сколько это обойдется?');
+console.log('Во сколько это обойдется?: ', question2);
+
+let question3 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+console.log('Какие обязательные ежемесячные расходы у вас есть?: ', question3);
+
+let question4 = prompt('Во сколько это обойдется?');
+console.log('Во сколько это обойдется?: ', question4);
+
+let budgetMonth = money-question2-question4;
+console.log('доход за месяц, учитывая обязательные расходы: ', budgetMonth);
+
+let mission = 65000;
+mission = parseFloat(Math.ceil(65000 / budgetMonth));
+console.log('за сколько месяцев будет достигнута цель: ', mission);
+
+let budgetDay = parseFloat(Math.floor(budgetMonth / 30));
+console.log('бюджет в день: ', budgetDay);
+
+if (budgetDay > 800) {
+  console.log('Высокий доход');
+} else if(budgetDay > 300 & budgetDay < 800){
+  console.log('Средний уровень дохода');
+} else if(budgetDay > 0 & budgetDay < 300){
+  console.log('Низкий уровень дохода');
+} else {
+  console.log('Что то пошло не так');
+} 
 
