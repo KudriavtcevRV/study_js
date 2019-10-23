62c3325e26e674121d88502ebb78f5b399a09f08
KudriavtcevRV <ruslan_kudriavtcev@mail.ru>
2019-10-23 22:29:04 +0300

    Второе задание 4-го урока

---
+++ js/lesson4-2.js
@@ -0,0 +1,8 @@
+function func(x){
+  if (typeof x !== 'string') {
+    alert('Вы ввели не строку');
+  } else if (typeof x === 'string'){
+    console.log(x.trim().substr(0, 30) + '...');
+  }
+}
+func('          Если строка более 30 знаков - то после 30го символа ');
